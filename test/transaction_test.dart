0599658a3d7f0f97467f2721846c9c1f78fa7a5d
saiy2k <saiy2k@gmail.com>
2023-11-15 14:44:34 +0530

    Text Test data 1/n

---
+++ test/transaction_test.dart
@@ -0,0 +1,11 @@
+import 'package:bb_mobile/_pkg/wallet/transaction.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('getTransaction()', () {
+    test('should return synced transaction list', () {
+      final WalletTx wtx = WalletTx();
+      // wtx.getTransactions(wallet: wallet, bdkWallet: bdkWallet);
+    });
+  });
+}
