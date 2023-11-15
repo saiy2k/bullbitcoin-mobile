1174f20f8fae61b9b73f03a7db322a7e43047b47
saiy2k <saiy2k@gmail.com>
2023-11-15 18:09:38 +0530

    Txt Test data 2/n

--- test/transaction_test.dart
+++ test/transaction_test.dart
@@ -1,11 +1,28 @@
 import 'package:bb_mobile/_pkg/wallet/transaction.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'transaction_test.data.dart';
+
 void main() {
   group('getTransaction()', () {
-    test('should return synced transaction list', () {
+    test('should return synced transaction list', () async {
       final WalletTx wtx = WalletTx();
-      // wtx.getTransactions(wallet: wallet, bdkWallet: bdkWallet);
+      final testWallet = getTestWallet();
+      /*
+      final bdkWallet = await bdk.Wallet.create(
+        descriptor: await bdk.Descriptor.create(
+          descriptor: 'dummydescriptor',
+          network: bdk.Network.Testnet,
+        ),
+        network: bdk.Network.Testnet,
+        databaseConfig:
+            const bdk.DatabaseConfig.sqlite(config: bdk.SqliteDbConfiguration(path: '')),
+      );
+      */
+      final bdkWallet = FakeBdkWallet();
+      final (syncedWallet, syncErr) =
+          await wtx.getTransactions(wallet: testWallet, bdkWallet: bdkWallet);
+      print(syncedWallet?.transactions[2]);
     });
   });
 }
